d86a1e8fba2aaf64cdc21b7ce2d672dc1b30325c
ggjiny <jnss599@gmail.com>
2024-07-18 14:19:42 +0900

    test: 센트리 테스트여

--- app/sentry-example-page/page.tsx
+++ app/sentry-example-page/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import * as Sentry from '@sentry/nextjs';
 import Head from 'next/head';
 
 export default function Page() {
@@ -35,32 +36,35 @@ export default function Page() {
         </h1>
 
         <p>Get started by sending us a sample error:</p>
-        {/* <button
+        <button
           type="button"
           style={{
-            padding: "12px",
-            cursor: "pointer",
-            backgroundColor: "#AD6CAA",
-            borderRadius: "4px",
-            border: "none",
-            color: "white",
-            fontSize: "14px",
-            margin: "18px",
+            padding: '12px',
+            cursor: 'pointer',
+            backgroundColor: '#AD6CAA',
+            borderRadius: '4px',
+            border: 'none',
+            color: 'white',
+            fontSize: '14px',
+            margin: '18px',
           }}
           onClick={async () => {
-            await Sentry.startSpan({
-              name: 'Example Frontend Span',
-              op: 'test'
-            }, async () => {
-              const res = await fetch("/api/sentry-example-api");
-              if (!res.ok) {
-                throw new Error("Sentry Example Frontend Error");
+            await Sentry.startSpan(
+              {
+                name: 'Example Frontend Span',
+                op: 'test',
+              },
+              async () => {
+                const res = await fetch('/api/sentry-example-api');
+                if (!res.ok) {
+                  throw new Error('Sentry Example Frontend Error');
+                }
               }
-            });
+            );
           }}
         >
           Throw error!
-        </button> */}
+        </button>
 
         <p>
           Next, look for the error on the{' '}
