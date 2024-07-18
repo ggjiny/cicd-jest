0ef45cf8ff2f7d2ea9307c1375c6f4c006186c0c
ggjiny <jnss599@gmail.com>
2024-07-18 16:09:45 +0900

    코드코브 테스트

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 export const metadata = {
-  title: "App Router",
+  title: 'App Router',
 };
 
 export default function Page() {
-  return <h1>App Router</h1>;
+  return (
+    <div>
+      <button>앙녕하세영</button>
+      <h2>코드 코브 연습중</h2>
+      <p> 되나용???</p>
+    </div>
+  );
 }
