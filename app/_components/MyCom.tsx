048f027f39319f9e4c9ef226115d03b46dc06232
ggjiny <jnss599@gmail.com>
2024-07-18 16:28:22 +0900

    test: 테스트 코드 추가

---
+++ app/_components/MyCom.tsx
@@ -0,0 +1,12 @@
+function MyCom() {
+  const number = 1;
+
+  return (
+    <div>
+      <h1>{number}</h1>
+      <p>{number + 1}</p>
+      <h1>{number}</h1>
+      <p>{number + 1}</p>
+    </div>
+  );
+}
